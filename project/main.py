7f55bafec117db4b99c8ede9e5a31045f1abdc78
tomsolarino <tomster954@gmail.com>
2021-08-07 18:47:55 +1000

    basic game over loop and score

--- project/main.py
+++ project/main.py
@@ -9,7 +9,8 @@ elif os.name == 'posix':
 
 
 # Global Variables
-snake_body_parts = [[0, 0], [0, 1]]
+playing = False
+snake_body_parts = [[0, 0]]
 snake_head_dir = [1, 0]  # The snakes current direction of motion
 snake_food_loc = [1, 0]  # The grid location of the snakes next food piece
 cols, rows = (20, 10)  # The size of the map / playground
@@ -70,19 +71,65 @@ def ClearScreen():
         os.system('clear')
 
 
+# Called if the player collides with themself or the edge of the map
+def game_over():
+    ClearScreen()
+    ClearPlayground()
+    global snake_body_parts
+
+    center_text(playground, 2, "GAME OVER!")
+    center_text(playground, 3, "SCORE " + str(len(snake_body_parts)))
+    center_text(playground, 6, "PRESS Y TO BEGIN")
+    center_text(playground, 7, "Q TO QUIT")
+
+    screen = ''
+    for row in range(rows):
+        for col in range(cols):
+            screen = screen + playground[row][col]
+        screen = screen + '\n'
+    print(screen)
+
+    if (should_play()):
+        snake_body_parts = [[0, 0]]
+        # Center the snake's head in the map
+        snake_body_parts[0][0] = round(rows / 2)  # row
+        snake_body_parts[0][1] = round(cols / 2)  # col
+        # Randomize the first piece of food
+        new_food_position()
+    else:
+        global playing
+        playing = False
+
+
+# checks if the game should end
+def check_game_over():
+    # Check that the snakes head didn't hit the edge of the map
+    if snake_body_parts[0][0] > rows - 2 or snake_body_parts[0][0] < 1 or snake_body_parts[0][1] > cols - 2 or snake_body_parts[0][1] < 1:
+        game_over()
+        return
+    # Check that the snake didn't collide with itself
+    for elem in snake_body_parts:
+        if snake_body_parts.count(elem) > 1:
+            game_over()
+            return
+
+
 # Updates the game
 def update_game(playground):
     if (snake_body_parts[0][0] + snake_head_dir[0] == snake_food_loc[0]) and (snake_body_parts[0][1] + snake_head_dir[1] == snake_food_loc[1]):
         new_food_position()
         snake_body_parts.append([0, 0])
 
+    # Move all the body parts of the snake forward
     for i in range(len(snake_body_parts)-1, -1, -1):
         if i == 0:
             snake_body_parts[i][0] += snake_head_dir[0]
             snake_body_parts[i][1] += snake_head_dir[1]
         else:
-           snake_body_parts[i][0] = snake_body_parts[i - 1][0]
-           snake_body_parts[i][1] = snake_body_parts[i - 1][1]
+            snake_body_parts[i][0] = snake_body_parts[i - 1][0]
+            snake_body_parts[i][1] = snake_body_parts[i - 1][1]
+
+    check_game_over()
 
 
 # Draws the game after it was updated
@@ -110,17 +157,17 @@ def draw_game(playground):
 # Will wait until either 'y' or 'q' is clicked
 def should_play():
     while(True):
+        global last_key_pressed
         if last_key_pressed in [121, 89, "y", "Y"]:  # (Y / y)
+            last_key_pressed = ''
             return True
         if last_key_pressed in [113, 81, "q", "Q", "esc"]:  # (Q / q / escape)
             return False
 
 
 # Create a new random position for the snakes food
+# Make sure that is ins't on the snake though!
 def new_food_position():
-    # snake_food_loc[0] = random.randint(1, rows - 2)
-    # snake_food_loc[1] = random.randint(1, cols - 2)
-
     new_loc = [random.randint(1, rows - 2), random.randint(1, cols - 2)]
 
     # if the food is going to appear where the snakes head is about to go
@@ -128,10 +175,13 @@ def new_food_position():
         new_food_position()
         return
 
+    # If the food is about to appear where another part of the snake exists
     for part in snake_body_parts:
         if part == new_loc:
             new_food_position()
             return
+
+    # Fianlly if all tests past set the new foods position
     snake_food_loc[0] = new_loc[0]
     snake_food_loc[1] = new_loc[1]
 
@@ -145,7 +195,8 @@ def on_press(key):
     except AttributeError:
         last_key_pressed = key.name
 
-    # Change the snakes position based off user input
+    # Change the snakes direction of motion based off user input
+    # Make sure the snake cannot go back on its self
     if last_key_pressed in ["left", "a", "A"] and snake_head_dir[1] != 1:
         snake_head_dir = [0, -1]
     elif last_key_pressed in ["right", "d", "D"] and snake_head_dir[1] != -1:
@@ -163,6 +214,7 @@ def main_gameplay_loop():
 
     # Checks if the user want to play or not
     if should_play():
+        global playing
         playing = True
         next_update = 0
 
@@ -179,7 +231,8 @@ def main_gameplay_loop():
                 next_update = time.time() + 1
                 update_game(playground)
                 draw_game(playground)
-
+    
+    ClearScreen()
 
 # This is the entry point for the main script
 if __name__ == '__main__':
